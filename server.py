ffe030266a5843ec016227195b519593bc661e94
ermissa <ermissa@itu.edu.tr>
2017-10-28 19:36:55 +0300

    turgut hocadanalinanlar

---
+++ server.py
@@ -0,0 +1,23 @@
+import datetime
+import os
+
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    now = datetime.datetime.now()
+    return render_template('home.html', current_time=now.ctime())
+
+
+if __name__ == '__main__':
+    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
+    if VCAP_APP_PORT is not None:
+        port, debug = int(VCAP_APP_PORT), False
+    else:
+        port, debug = 5000, True
+    app.run(host='0.0.0.0', port=port, debug=debug)
