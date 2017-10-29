a7b99bc5f10b07e5ce4db113a339845c91c9d2f8
ermissa <ermissa@itu.edu.tr>
2017-10-30 02:07:39 +0300

    /create_db'de bir hata var çözemedim

--- server.py
+++ server.py
@@ -1,17 +1,44 @@
 import datetime
 import os
-
+import traceback
+import logging
+import psycopg2 as dbapi
 from flask import Flask
 from flask import render_template
-
+from schools import Schools
 
 app = Flask(__name__)
+
+
+# def get_elephantsql_dsn(vcap_services):
+#     """Returns the data source name for ElephantSQL."""
+#     parsed = json.loads(vcap_services)
+#     uri = parsed["elephantsql"][0]["credentials"]["uri"]
+#     match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+#     user, password, host, _, port, dbname = match.groups()
+#     dsn = """user='{}' password='{}' host='{}' port={}
+#              dbname='{}'""".format(user, password, host, port, dbname)
+#     return dsn
+
+#for local
+dsn = """user='root' password='123' host='localhost' port=5000
+              dbname='db_yoklama'"""
 
 
 @app.route('/')
 def home_page():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
+
+@app.route('/create_db')
+def create_db():
+    try:
+        now = datetime.datetime.now()
+        school = Schools(dsn=dsn)
+        school.init_table()
+        return render_template('home.html', current_time=now.ctime())
+    except Exception as e:
+        logging.error(str(e))
 
 
 if __name__ == '__main__':
